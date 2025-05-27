f7cfd1f968a1991fb1aebe435bd637c634d8dad4
JulienDel59 <juliendelrue59140@gmail.com>
2025-05-27 13:36:32 +0200

    exo3 JS

---
+++ programming/Javascipt/exo3/page.js
@@ -0,0 +1,12 @@
+let prenom = "john";
+let nom = " doe";
+NomEntier = prenom + nom;
+console.log(NomEntier);
+
+
+
+let sujet = "Le chat" ;
+let verbe = " mange";
+let objet = " la souris";
+phrase = sujet + verbe + objet;
+console.log(phrase);
