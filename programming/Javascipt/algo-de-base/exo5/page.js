2e7331113e1c9007c328fa1857f37e669ccb717f
JulienDel59 <juliendelrue59140@gmail.com>
2025-05-27 15:47:57 +0200

    exo5 JS

--- programming/Javascipt/algo-de-base/exo5/page.js
+++ programming/Javascipt/algo-de-base/exo5/page.js
@@ -1,3 +1,11 @@
 let tableau = [1, 2, 3, 4, 5];
 
 console.log(tableau);
+
+// :::::::::
+
+let tableau2 = [];
+tableau2.push(10,20,30);
+let firstElment =  tableau2.shift();
+console.log(tableau2);
+
