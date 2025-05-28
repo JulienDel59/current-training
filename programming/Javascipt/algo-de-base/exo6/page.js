81620875e594e1dff693ab9e7ecbf975fe430e2f
JulienDel59 <juliendelrue59140@gmail.com>
2025-05-28 08:49:20 +0200

    exo6 JS

--- programming/Javascipt/algo-de-base/exo6/page.js
+++ programming/Javascipt/algo-de-base/exo6/page.js
@@ -6,11 +6,17 @@ let personne = {
 
 console.log(personne);
 
+// :::::
+
 let compteBancaire = {
     titulaire: 'John Doe',
     solde : 1000,
 };
 
-console.log(compteBancaire);
+console.log("Solde initial :",compteBancaire.solde);
+
+compteBancaire.solde += 500;
+
+console.log("Nouveau solde après ajout de 500 :",compteBancaire)
 
     
