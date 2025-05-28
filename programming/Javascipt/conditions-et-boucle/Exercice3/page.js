758ca56ca7b63ac1f1f4307f6d1bf5183740af0c
JulienDel59 <juliendelrue59140@gmail.com>
2025-05-28 16:29:53 +0200

    exo3 CetB

---
+++ programming/Javascipt/conditions-et-boucle/Exercice3/page.js
@@ -0,0 +1,20 @@
+// Génère un nombre aléatoire entre 1 et 100
+const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+let guess = 0; // initialisation
+let attempts = 0; // nombre de tentatives
+
+while (guess !== secretNumber) {
+  // Demander à l'utilisateur de deviner
+  guess = parseInt(prompt("Devinez un nombre entre 1 et 100 :"));
+
+  attempts++;
+
+  if (guess < secretNumber) {
+    alert("Trop petit !");
+  } else if (guess > secretNumber) {
+    alert("Trop grand !");
+  } else {
+    alert("Bravo ! Vous avez trouvé le nombre en " + attempts + " tentative(s).");
+  }
+}
