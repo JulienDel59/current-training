a04358923243c3c5a57d0706ed7a16d40b5e61c8
JulienDel59 <juliendelrue59140@gmail.com>
2025-05-30 11:04:00 +0200

    exo3

---
+++ programming/Javascipt/fonction.md/exo3/page.js
@@ -0,0 +1,11 @@
+const fruits = ['Apple', 'Banana', 'Apple', 'Orange', 'Banana', 'Apple'];
+
+function removeDuplicates(fruits) {
+     return [...new Set (fruits)];
+}
+
+
+ console.log(removeDuplicates(fruits));
+
+
+
