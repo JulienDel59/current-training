591db6b1024c13d373e654788ee7edba722e5084
JulienDel59 <juliendelrue59140@gmail.com>
2025-05-30 11:46:05 +0200

    exo4 fonction.md

---
+++ programming/Javascipt/fonction.md/exo4/page.js
@@ -0,0 +1,12 @@
+const numbers = [1,2,3,4,5]
+let value = 6;
+
+
+
+function containsElement (numbers,value) {
+    return numbers.includes(value);
+}
+
+console.log(containsElement(numbers,value));
+
+
