f67189c95fd0909f987e69f10eea5c5a80a9c37e
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-03 11:27:04 +0200

    suite morpion

--- programming/Javascipt/project-morpion/js.js
+++ programming/Javascipt/project-morpion/js.js
@@ -2,11 +2,15 @@
 let currentPlayer = 'X'; // Le joueur X commence
 let gameBoard = ['', '', '', '', '', '', '', '', '']; // Tableau pour suivre l'état des cases
 let gameActive = true; // Variable pour savoir si le jeu est encore actif
+let scoreX = 0;
+let scoreO = 0;
 
 // Sélectionner toutes les cases
 const cells = document.querySelectorAll('.case');
 
-
+// Sélectionner les éléments affichant les scores
+const scoreXDisplay = document.getElementById('scoreX');
+const scoreODisplay = document.getElementById('scoreO');
 
 // Fonction pour gérer les clics sur les cases
 function handleCellClick(event) {
@@ -22,6 +26,14 @@ function handleCellClick(event) {
 
     // Vérifier si quelqu'un a gagné
     if (checkWinner()) {
+            if (currentPlayer === 'X') {
+        scoreX++;
+        scoreXDisplay.textContent = scoreX;
+       } else {
+        scoreO++;
+        scoreODisplay.textContent = scoreO;
+       } 
+
         setTimeout(() => {
             alert(`${currentPlayer} a gagné !`);
             resetGame(); // Réinitialiser le jeu après la victoire
