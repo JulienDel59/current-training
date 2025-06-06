e94a4e0e681ac7b46a92541f52ca369a8d642d76
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-06 15:35:00 +0200

    correction

--- programming/Javascipt/fonction.md/exo4/page.js
+++ programming/Javascipt/fonction.md/exo4/page.js
@@ -1,27 +1,14 @@
-const numbers = [1,2,3,4,5]
-let value = 1;
+  const numbers = [1,2,3,4,5]
+let value = 6;
 
 
 
 function containsElement (numbers,value) {
-    return numbers.includes(value);
-}
+    return numbers.includes(value);   }
 
-console.log(containsElement(numbers,value));
-
-
-// ::::::::::
+  console.log(containsElement(numbers,value));
 
 
- function containsElement(numbers, value) {
- for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] === value) {
-     return true;
-   }
- }
-    return false;
- }
 
-console.log(containsElement(numbers,value));
 
 
