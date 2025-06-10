9255a16a3c2898f2cbe1aa1ab7c958119fdc00b6
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-10 09:02:16 +0200

    exemple de switch

--- programming/Javascipt/switch/js.js
+++ programming/Javascipt/switch/js.js
@@ -4,15 +4,19 @@ switch (day) {
     case 1:
         console.log("Spaghetti Bolognaise");
     break;
+
      case 2:
         console.log("Poulet rôti");
     break;
+
      case 3:
         console.log("Poisson pané");
     break;
+
      case 4:
         console.log(" Salade composée");
     break;
+    
      case 5:
         console.log("Pizza Margherita");
     break;
