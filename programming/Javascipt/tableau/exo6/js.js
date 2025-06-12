ffca2e683412e971369efbee335b4ec0597571e3
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-12 11:43:08 +0200

    exo6 tab

--- programming/Javascipt/tableau/exo6/js.js
+++ programming/Javascipt/tableau/exo6/js.js
@@ -2,18 +2,19 @@ const nombres = [1, 5, 12, 8, 130, 44];
 
 nombres.forEach(function(nombre) {
     console.log("Le nombre est:" + nombre);
-    
 });
 
-const filtered = nombres.filter(function(nombre) {
+const grandNombres = nombres.filter(function(nombre) {
   return nombre > 10;
 });
 
-console.log("Voici les nombres du tableau supperieur a 10:" + filtered);
+console.log("Voici les nombres du tableau supperieur a 10:" + grandNombres);
 
 const doubled = nombres.map(function(nombre) {
   return nombre * 2;
 });
 
 console.log("le double de chaque nombres est:" + doubled)
+
+
 
