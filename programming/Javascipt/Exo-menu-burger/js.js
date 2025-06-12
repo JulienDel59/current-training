58521a1bd268c9d06abb32fcc9fde7e8d4776fc1
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-12 15:48:53 +0200

    exo menu burger

--- programming/Javascipt/Exo-menu-burger/js.js
+++ programming/Javascipt/Exo-menu-burger/js.js
@@ -4,15 +4,15 @@ const menu = document.getElementById('menu');
 
 menuBurger.addEventListener('click', () => {
   if (menu.classList.contains('active')) {
-    // Démarre fermeture avec disparition des items
+    
     menu.classList.add('closing');
 
-    // Après 0.5s + délai max 0.3s + un petit buffer, enlève classes
+    
     setTimeout(() => {
       menu.classList.remove('active', 'closing');
-    }, 900); // 900ms laisse le temps aux items de disparaître puis menu ferme
+    }, 900); 
   } else {
-    // Ouverture directe
+    
     menu.classList.add('active');
   }
 
