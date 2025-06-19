d22205f48914a3953d9699be53fa096111afcd13
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-19 09:59:29 +0200

    ...

--- programming/Javascipt/exoAPI/js.js
+++ programming/Javascipt/exoAPI/js.js
@@ -1,11 +1,12 @@
-fetch("https://trouve-mot.fr/api/random")
-    .then((response) => response.json())
-    .then((words) => console.log(words))
-
-fetch("https://trouve-mot.fr/api/random/2")
-    .then((response) => response.json())
-    .then((words) => console.log(words))
-
-    fetch("https://trouve-mot.fr/api/random/10")
-    .then((response) => response.json())
-    .then((words) => console.log(words))
+fetch("https://trouve-mot.fr/api/random/10")
+  .then((response) => response.json())
+  .then((words) => {
+    console.log(words);
+    const ul = document.createElement('ul'); // Crée une liste
+    words.forEach(word => {
+      const li = document.createElement('li'); // Crée un élément de liste
+      li.textContent = item.word; // Met le mot dans l'élément
+      ul.appendChild(li); // Ajoute l'élément à la liste
+    });
+    document.body.appendChild(ul); // Ajoute la liste dans le body
+  });
