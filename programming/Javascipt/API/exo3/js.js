c705d944239c5a34ad86ebfc01d45785fb9f0dc6
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-24 13:16:18 +0200

    suite exo 3 API

--- programming/Javascipt/API/exo3/js.js
+++ programming/Javascipt/API/exo3/js.js
@@ -1,2 +1,11 @@
 const btnSubmit = document.querySelector('.btnSubmit');
 const btnText = document.querySelector('btnText');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.querySelector('.btnText');
+    let text = name.value;
+    console.log(text);
+})
+
+
