a21d5b95466e7023307ecc5d69960caa2c6ec265
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-24 14:20:04 +0200

    suite exo3 API

--- programming/Javascipt/API/exo3/js.js
+++ programming/Javascipt/API/exo3/js.js
@@ -1,11 +1,61 @@
-const btnSubmit = document.querySelector('.btnSubmit');
-const btnText = document.querySelector('btnText');
+// const btnSubmit = document.querySelector('.btnSubmit');
+// const btnText = document.querySelector('.btnText');
+// const firstName = document.querySelector('name');
+// const lastName = document.querySelector('name');
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const btnText = document.querySelector('.btnText');
+//     let text = btnText.value;
+//     console.log(text);
+
+
+//   fetch('https://685a75ec9f6ef96111567ea2.mockapi.io/utilisateur', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       prenom: firstName,
+//       nom: lastName
+//     })
+//   })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     alert(`Utilisateur créé avec succès : ${JSON.stringify(data)}`);
+//   })
+
+// });
+
+const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const name = document.querySelector('.btnText');
-    let text = name.value;
-    console.log(text);
-})
 
+    // Récupération des valeurs des champs
+    const firstName = document.querySelector('input[name="FirstName"]').value;
+    const lastName = document.querySelector('input[name="LastName"]').value;
 
+    console.log(`Prénom: ${firstName}, Nom: ${lastName}`);
+
+    // Envoi des données à l'API
+    fetch('https://685a75ec9f6ef96111567ea2.mockapi.io/utilisateur', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            Prenom: firstName,
+            Nom: lastName
+        })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        alert(`Utilisateur créé avec succès : ${JSON.stringify(data)}`);
+    })
+    .catch((error) => {
+        console.error('Erreur lors de l’envoi des données :', error);
+    });
+});
