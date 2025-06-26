8d2ff4e40786cc4c248d13c3c4658b95038ac36e
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-26 13:28:06 +0200

    suite exo2 formData

---
+++ programming/Javascipt/Form-data/exo2/js.js
@@ -0,0 +1,10 @@
+const passwordOutput = document.getElementById('password-output')
+const rangeLenght = document.getElementById('password-length')
+const displayLenght = document.getElementById('display-password-length')
+
+const lowercaseCheckbox = document.getElementById("lowercase");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolsCheckbox = document.getElementById("symbols"); 
+
+const Buntton = document.getElementById('generateButton')
