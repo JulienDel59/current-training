8e9899d9f4549575a0bac09a22ef82c5971cbdda
JulienDel59 <juliendelrue59140@gmail.com>
2025-06-26 14:16:07 +0200

    exo2 formData

--- programming/Javascipt/Form-data/exo2/js.js
+++ programming/Javascipt/Form-data/exo2/js.js
@@ -1,5 +1,5 @@
 const passwordOutput = document.getElementById('password-output')
-const rangeLenght = document.getElementById('password-length')
+const rangeInput = document.getElementById('password-length')
 const displayLenght = document.getElementById('display-password-length')
 
 const lowercaseCheckbox = document.getElementById("lowercase");
@@ -7,15 +7,34 @@ const uppercaseCheckbox = document.getElementById("uppercase");
 const numbersCheckbox = document.getElementById("numbers");
 const symbolsCheckbox = document.getElementById("symbols"); 
 
-const Buntton = document.getElementById('generateButton')
+const button = document.getElementById('generateButton')
 
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+[]{}|;:,.<>?/~`-=";
 
-rangeLenght.addEventListener("input" , () => {
-    displayLenght.value = rangeLenght.value;
+rangeInput.addEventListener("input" , () => {
+    displayLenght.value = rangeInput.value;
 });
 
+function  generatePassword () {
+    const length = parseInt(rangeInput.value);
+    let characters = "" ;
+
+    if (lowercaseCheckbox.checked) characters = `${characters}${lowercaseLetters}`;
+    if (uppercaseCheckbox.checked) characters = `${characters}${uppercaseLetters}`;
+    if (numbersCheckbox.checked) characters += `${characters}${numbers}`;
+    if (symbolsCheckbox.checked) characters += `${characters}${symbols}`;
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+     const randomChar = characters[Math.floor(Math.random() * characters.length)];
+     password += randomChar;
+  }
+
+  passwordOutput.value = password;
+}
+    
+button.addEventListener("click", generatePassword);
 
